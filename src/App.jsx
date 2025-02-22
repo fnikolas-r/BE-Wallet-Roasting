8605dd0e18a3b4bd500971c4e7a21d404cbaa8ac
IbnuAlfarezi <muh.ibnufarezi@gmail.com>
2025-02-22 20:23:59 +0800

    Input

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import './App.css'
+import Cards from './components/card'
 
 function App() {
 
   return (
     <>
     <div className="flex flex-col items-center pt-10">
+
         <div className="flex flex-row">
           <div className='font-[Mochiy_Pop_One] text-[36px] font-normal text-white'>Roasting</div>
           <div className='font-[Mochiy_Pop_One] text-[36px] font-normal text-[#e11d48]'>Wallet</div>
@@ -13,6 +15,10 @@ function App() {
           <div className='font-[dm_sans] text-[24px] font-normal text-white'>by</div>
           <div className='font-[dm_sans] text-[24px] font-normal text-[#f59e0b]'>@felas</div>
         </div>
+        <br />
+        {/* Cards */}
+        <Cards/>
+
     </div>
     </>
   )
