2f55e1e7b614d86cbeb81be2ae1ae8c441e2a5af
IbnuAlfarezi <muh.ibnufarezi@gmail.com>
2025-02-22 19:25:09 +0800

    Image

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,12 @@ function App() {
   return (
     <>
     <div className="flex flex-col items-center pt-10">
-        <div className="flex flex-row ">
-          <div className='font-[Mochiy_Pop_One] text-[36px] font-normal'>Roasting</div>
+        <div className="flex flex-row">
+          <div className='font-[Mochiy_Pop_One] text-[36px] font-normal text-white'>Roasting</div>
           <div className='font-[Mochiy_Pop_One] text-[36px] font-normal text-[#e11d48]'>Wallet</div>
         </div>
-        <div className="flex flex-row ">
-          <div className='font-[dm_sans] text-[24px] font-normal'>by</div>
+        <div className="flex flex-row">
+          <div className='font-[dm_sans] text-[24px] font-normal text-white'>by</div>
           <div className='font-[dm_sans] text-[24px] font-normal text-[#f59e0b]'>@felas</div>
         </div>
     </div>
